438d91f5503829511f2c5688bf9ced589d24075a
HannaM1401 <47866618+HannaM1401@users.noreply.github.com>
2022-12-08 03:14:17 -0600

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-undef, no-unused-vars */
+
+  let button1,
+  button2,
+  button3,
+  button4,
+  button5,
+  button6,
+  button7,
+  button8,
+  button9;
+
+  const multiplayer = new Tone.Players({
+    piano:
+    percussion:
+    woodwinds:
+    flute:
+    trumpet:
+    bells:
+  })
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, 500);
+  background(153);
+}
+
+function draw() {}
